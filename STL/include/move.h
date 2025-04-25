a6d45548f0ec8f50eb9e11368ae37f89bbfd7ea7
remydzn <163844542+remydzn@users.noreply.github.com>
2025-04-26 01:00:18 +0900

    updated comments

--- STL/include/move.h
+++ STL/include/move.h
@@ -1,4 +1,20 @@
 #pragma once
+
+/*
+* Custom move function implementation by Seungtack Lee
+* Author: Lee, Seungtack
+* GitHub: https://github.com/remydzn/cpp-study
+* Date: Apr 25, 2025
+*
+* <Authorship Verification>
+* This section exists to verify that this project was originally created by Seungtack Lee.
+* Please leave a star when you are cloning this code.
+*
+* Unauthorized use in academic or professional submissions without proper credit is prohibited.
+* If you received this code from a source other than my GitHub repository, please let me know:
+* https://github.com/remydzn
+*/
+
 #include "remove_reference.h"
 
 namespace ST
